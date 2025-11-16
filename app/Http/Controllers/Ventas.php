1eebcbc35cea92ec639a91f13cffb885e296e2b8
AndreaHerrera25 <andrea.herrera6279@alumnos.udg.mx>
2025-11-15 18:20:35 -0600

    Se agrega el Proyecto

---
+++ app/Http/Controllers/Ventas.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Ventas extends Controller
+{
+    public function index() {
+        return view('modules.ventas.index');
+    }
+
+    public function create() {
+        return view('modules.ventas.create');
+    }
+}
